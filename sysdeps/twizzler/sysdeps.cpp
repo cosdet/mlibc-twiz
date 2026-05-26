e9efddad8c14203d786d5ab16af2280832737ba8
Sean Weber <slweber@ucsc.edu>
2026-05-25 20:04:25 -0700

    fix: missing semicolon

--- sysdeps/twizzler/sysdeps.cpp
+++ sysdeps/twizzler/sysdeps.cpp
@@ -1066,10 +1066,10 @@ int sys_isatty(int fd) {
 
 int sys_ioctl(int fd, unsigned long request, void *arg, int *result) {
     SYSTRACE("sys_ioctl(fd=%d, request=%lu, arg=%p, result=%p)", fd, request, arg, result);
-    
-    *result = 0
 
-    switch(request) {
+	*result = 0;
+
+	switch(request) {
         case TIOCGWINSZ:
             return twz_error_errno(twz_rt_fd_get_config(fd, IO_REGISTER_WINSIZE, arg, sizeof(struct winsize)));
         case TIOCSWINSZ:
